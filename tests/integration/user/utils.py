4f179d97b9a976e1068b9a6a96695b4012b7ad8a
lubaskin <lubaskincorporation@gmail.com>
2025-12-08 00:59:12 +0300

    refactor(): rename ``client`` parameter of ``create_user`` function to ``api_client``

--- tests/integration/user/utils.py
+++ tests/integration/user/utils.py
@@ -2,9 +2,9 @@ from crudik.adapters.api_client import APIClient
 from crudik.entities.common.identifiers import UserId
 
 
-async def create_user(client: APIClient) -> UserId:
+async def create_user(api_client: APIClient) -> UserId:
     """Create a user via API and return its id."""
-    response = await client.create_user()
+    response = await api_client.create_user()
     user_id = response.assert_status(200).ensure_ok().id
 
     return user_id
